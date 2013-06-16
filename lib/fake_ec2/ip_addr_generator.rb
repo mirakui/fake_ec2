a8cab8b34b08d3bda361c620361e546617803bb6
Issei Naruta <naruta@cookpad.com>
2013-06-16 16:44:21 +0900

    Add IpAddrGenerator

---
+++ lib/fake_ec2/ip_addr_generator.rb
@@ -0,0 +1,26 @@
+require 'ipaddr'
+module FakeEc2
+  class IpAddrGenerator
+    def initialize
+      @mutex = Mutex.new
+      reset!
+    end
+
+    def reset!
+      @counters = Hash.new {|h, k| h[k] = 0 }
+    end
+
+    def generate(cidr, key=nil)
+      key ||= cidr
+      @mutex.synchronize do
+        IPAddr.new(cidr).to_range.each_with_index do |ip, i|
+          next if i <= @counters[key]
+          next if ip.to_s =~ /\.(0|255)$/
+          @counters[key] = i
+          return ip
+        end
+        raise RuntimeError, "CIDR #{key} runs out"
+      end
+    end
+  end
+end
---
+++ spec/lib/fake_ec2/ip_addr_generator_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'fake_ec2/ip_addr_generator'
+
+describe FakeEc2::IpAddrGenerator do
+  let!(:generator) { FakeEc2::IpAddrGenerator.new }
+  before { generator.reset! }
+
+  describe '#generate' do
+    specify do
+      generator.generate('10.0.0.0/17').should == '10.0.0.1'
+      generator.generate('10.0.0.0/17').should == '10.0.0.2'
+      generator.generate('10.0.0.0/17').should == '10.0.0.3'
+    end
+
+    it 'do not generate invalid ip address like 10.0.0.255' do
+      254.times do
+        generator.generate('10.0.0.0/17')
+      end
+      generator.generate('10.0.0.0/17').should == '10.0.1.1'
+    end
+
+    it 'runs out CIDR' do
+      generator.generate('10.0.0.0/31').should == '10.0.0.1'
+      expect {
+        generator.generate('10.0.0.0/31')
+      }.to raise_error
+    end
+  end
+end
