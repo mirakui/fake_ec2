4758f33658cfc3732bda7d94f3115934f744e514
Issei Naruta <naruta@cookpad.com>
2013-06-22 15:12:46 +0900

    Implement RunInstances

--- lib/fake_ec2/action.rb
+++ lib/fake_ec2/action.rb
@@ -1,8 +1,13 @@
+require 'fake_ec2'
+
 module FakeEc2
   module Action
     autoload 'RunInstances', 'fake_ec2/action/run_instances'
 
     class Base
+      def space
+        FakeEc2.space
+      end
     end
   end
 end
--- lib/fake_ec2/action/run_instances.rb
+++ lib/fake_ec2/action/run_instances.rb
@@ -1,8 +1,21 @@
+require 'fake_ec2/model/instance'
 require 'fake_ec2/action'
+
 module FakeEc2
   module Action
     class RunInstances < Base
       def run(params)
+        instances = []
+        params[:max_count].to_i.times do |i|
+          instances << FakeEc2::Model::Instance.new
+        end
+        space.instances.concat instances
+        {
+          request_id: space.id_generator.generate_request_id,
+          owner_id: nil,
+          group_set: nil,
+          instances_set: instances.map(&:to_h)
+        }
       end
     end
   end
