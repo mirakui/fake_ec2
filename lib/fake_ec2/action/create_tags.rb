b77e2d30ba4bcaa8bc39256a06b313627f8e6317
Issei Naruta <naruta@cookpad.com>
2013-08-03 13:17:42 +0900

    Add CreateTags

---
+++ lib/fake_ec2/action/create_tags.rb
@@ -0,0 +1,23 @@
+require 'fake_ec2/model'
+require 'fake_ec2/action'
+
+module FakeEc2
+  module Action
+    class CreateTags < Base
+      def run(params)
+        resource_ids = params[:resource_id]
+        resource_ids.each do |resource_id|
+          tags = params[:tag]
+          tags.each do |tag|
+            space.tags << Model::Tag.new(
+              resource_id: resource_id,
+              key: tag[:key],
+              value: tag[:value]
+            )
+          end
+        end
+        generate_result return: 'true'
+      end
+    end
+  end
+end
---
+++ spec/lib/fake_ec2/action/create_tags_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'fake_ec2/action'
+require 'fake_ec2/model'
+
+describe FakeEc2::Action::CreateTags do
+  let(:action) { described_class.new }
+  let(:space) { FakeEc2.space }
+  before { space.clear }
+
+  describe '#run' do
+    subject!(:result) do
+      action.run(
+        resource_id: %w(ami-001 i-001),
+        tag: [
+          { key: 'Key1', value: 'Value1' },
+          { key: 'Key2', value: 'Value2' }
+        ]
+      )
+    end
+
+    it { expect(result[:return]).to eq('true') }
+    it { expect(space.tags).to have(4).items }
+    it { expect(space.tags.first.resource_id).to eq('ami-001') }
+    it { expect(space.tags.first.key).to eq('Key1') }
+    it { expect(space.tags.first.value).to eq('Value1') }
+  end
+end
+
