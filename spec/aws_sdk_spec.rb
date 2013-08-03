1baeb483a1d8d18a34c651770402e8581bf470af
Issei Naruta <naruta@cookpad.com>
2013-08-03 11:08:25 +0900

    Add examples around tags

--- spec/aws_sdk_spec.rb
+++ spec/aws_sdk_spec.rb
@@ -17,4 +17,18 @@ describe 'AWS::Ec2 handler' do
       ec2.instances.to_a.should have(2).instances
     end
   end
+
+  describe 'Tags' do
+    before do
+      ec2.instances.create image_id: 'ami-001', count: 2
+      ec2.instances.each_with_index do |ins, i|
+        ins.tags.set 'Name' => "host#{i}", 'Role' => "role#{i}"
+      end
+    end
+
+    it { expect(ec2.instances[0].tags['Name']).to eq('host0') }
+    it { expect(ec2.instances[0].tags['Role']).to eq('role0') }
+    it { expect(ec2.instances[1].tags['Name']).to eq('host1') }
+    it { expect(ec2.instances[1].tags['Role']).to eq('role1') }
+  end
 end
