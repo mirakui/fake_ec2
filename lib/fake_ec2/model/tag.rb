077ac093fda5f2b0b8159ef7387a348e24f86249
Issei Naruta <naruta@cookpad.com>
2013-08-03 12:35:07 +0900

    Add tag model

---
+++ lib/fake_ec2/model/tag.rb
@@ -0,0 +1,30 @@
+require 'fake_ec2'
+require 'fake_ec2/model'
+
+module FakeEc2
+  module Model
+    class Tag < Base
+      # customer-gateway | dhcp-options | image | instance | internet-gateway | network-acl |
+      # network-interface | reserved-instances | route-table | security-group | snapshot |
+      # spot-instances-request | subnet | volume | vpc | vpn-connection | vpn-gateway
+      RESOURCE_TYPE_PREFIXES = Hash[*%w(
+        i    instance
+        ami  image
+        sg   security-group
+        vol  volume
+        snap snapshot
+        vpc  vpc
+        eni  network-interface
+      )].freeze
+
+      field :resource_id
+      field :key
+      field :value
+
+      def resource_type
+        prefix = resource_id.split('-').first
+        RESOURCE_TYPE_PREFIXES[prefix]
+      end
+    end
+  end
+end
